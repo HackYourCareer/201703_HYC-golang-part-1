ace0bb66833613fbcd6e00810b737db59eb74f36
Szecowka, Adam <adam.szecowka@sap.com>
2017-03-28 15:20:15 +0200

    barcamp link

--- src/million.go
+++ src/million.go
@@ -1,17 +1,18 @@
-package main 
+package main
+
 import "sync"
 import "time"
 import "fmt"
 import "math/rand"
 
 func main() {
-		wg := sync.WaitGroup{}
+	wg := sync.WaitGroup{}
 	bef := time.Now()
 	const len = 1000000 // million
 	wg.Add(len)
 	var out [len]int32
 	for i := 0; i < len; i++ {
-		 go func(idx int) {
+		go func(idx int) {
 			out[idx] = rand.Int31n(1000)
 			wg.Done()
 		}(i)
